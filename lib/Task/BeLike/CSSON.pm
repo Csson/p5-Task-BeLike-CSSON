fadb408533ff92d4b9c95ed0a9684331aec02d6a
Erik Carlsson <info@code301.com>
2016-01-10 16:17:56 +0000

    Define contents in .pm

--- lib/Task/BeLike/CSSON.pm
+++ lib/Task/BeLike/CSSON.pm
@@ -1,13 +1,16 @@
+use strict;
+use warnings;
+
 package Task::BeLike::CSSON { }
-$Task::BeLike::CSSON::VERSION = '0.0003';
+
+# ABSTRACT: Be like...
+# VERSION
+
 1;
+
 __END__
 
-=encoding utf-8
-
-=head1 NAME
-
-Task::BeLike::CSSON - Stuff I need
+=pod
 
 =head1 SYNOPSIS
 
@@ -32,21 +35,210 @@ Task::BeLike::CSSON - Stuff I need
 
 =head1 TASK CONTENTS
 
-See cpanfile.
+=pkgroup Stuff
 
-=head1 AUTHOR
+=pkg Archive::Tar::Wrapper
 
-Erik Carlsson E<lt>info@code301.comE<gt>
+=pkg Authen::Passphrase
 
-=head1 COPYRIGHT
+=pkg CAM::PDF
 
-Copyright 2014- Erik Carlsson
+=pkg Class::XSAccessor
 
-=head1 LICENSE
+=pkg CLI::Helpers
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=pkg Config::Hash
 
-=head1 SEE ALSO
+=pkg Config::JSON
+
+=pkg Config::MVP
+
+=pkg Crypt::Eksblowfish
+
+=pkg Curses::UI
+
+=pkg Data::Dump::Streamer
+
+=pkg Data::GUID
+
+=pkg Data::UUID
+
+=pkg DateTime
+
+=pkg DBD::mysql
+
+=pkg DBI
+
+=pkg DBIx::Class
+
+=pkg DBIx::Class::Candy
+
+=pkg DBIx::Class::Helpers
+
+=pkg DBIx::Class::EncodedColumn
+
+=pkg DBIx::Class::InflateColumn::DateTime::Duration
+
+=pkg DBIx::Class::InflateColumn::DateTime::WithTimeZone
+
+=pkg DBIx::Class::Migration
+
+=pkg Dist::Zilla
+
+=pkg Dist::Zilla::Plugin::Authority
+
+=pkg Dist::Zilla::Plugin::ChangeStats::Git
+
+=pkg Dist::Zilla::Plugin::EOLTests
+
+=pkg Dist::Zilla::Plugin::Git
+
+=pkg Dist::Zilla::Plugin::MinimumPerlFast
+
+=pkg Dist::Zilla::Plugin::NextVersion::Semantic
+
+=pkg Dist::Zilla::Plugin::Test::EOL
+
+=pkg Dist::Zilla::Plugin::Test::NoTabs
+
+=pkg Dist::Zilla::Plugin::Test::TrailingSpace
+
+=pkg Dist::Zilla::Plugin::UploadToStratopan
+
+=pkg Dist::Zilla::Role::PluginBundle::PluginRemover
+
+=pkg Dist::Zilla::Util::ExpandINI
+
+=pkg Dist::Iller
+
+=pkg Dist::Iller::Config::Author::CSSON
+
+=pkg Eponymous::Hash
+
+=pkg Exception::Class
+
+=pkg experimental
+
+=pkg File::ReadBackwards
+
+=pkg Games::2048
+
+=pkg GD
+
+=pkg Kavorka
+
+=pkg Lexical::Var
+
+=pkg List::AllUtils
+
+=pkg Math::Round
+
+=pkg MetaCPAN::Clients
+
+=pkg Method::Signatures
+
+=pkg Minilla
+
+=pkg Modern::Perl
+
+=pkg Mojolicious
+
+=pkg Mojolicious::Plugin::Authorization
+
+=pkg Moops
+
+=pkg Moose
+
+=pkg MooseX::Aliases
+
+=pkg MooseX::App
+
+=pkg MooseX::App::Cmd
+
+=pkg MooseX::Getopt
+
+=pkg MooseX::NonMoose
+
+=pkg MooseX::Object::Pluggable
+
+=pkg MooseX::Types
+
+=pkg MooseX::XSAccessor
+
+=pkg MooX::HandlesVia
+
+=pkg Net::Twitter
+
+=pkg PDF::API2
+
+=pkg Perl::Critic
+
+=pkg Pod::Weaver::Section::SeeAlso
+
+=pkg PPI::HTML
+
+=pkg Redis::Fast
+
+=pkg Regexp::Grammars
+
+=pkg SQL::Translator
+
+=pkg String::Markov
+
+=pkg String::Random
+
+=pkg String::Trim
+
+=pkg Syntax::Collection::Basic
+
+=pkg Syntax::Collector
+
+=pkg Term::ANSIColor
+
+=pkg Term::ReadLine::Gnu
+
+=pkg Test::Manifest
+
+=pkg Test::mysqld
+
+=pkg Test::Pod
+
+=pkg Test::Trap
+
+=pkg Throwable::X
+
+=pkg ToolSet
+
+=pkg Type::Tiny
+
+=pkg Type::Tiny::XS
+
+=pkg Types::URI
+
+=pkg URI::Find
+
+=pkg Variable::Magic
+
+=pkg CPAN::Changes
+
+=pkg Module::CPANfile
+
+=pkg Reindeer
+
+=pkg Dist::Zilla::Plugin::Test::EOF
+
+=pkg Pod::Markdown
+
+=pkg IO::Scalar
+
+=pkg Data::Perl
+
+=pkg Data::Visitor::Callback
+
+=pkg Net::OAuth
+
+=pkg IO::Interactive
+
+=pkg DateTime::Tiny
 
 =cut
